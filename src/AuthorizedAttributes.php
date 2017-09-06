d731e56ffbd4580416b50dd9a856a8eabcf73bf4
Jari Pekkala <jari.pekkala@outlook.com>
2017-09-06 10:43:28 +0300

    init

---
+++ src/AuthorizedAttributes.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Salomoni;
+
+use Illuminate\Support\Facades\Gate;
+
+trait AuthorizedAttributes
+{
+    /**
+     * Get the hidden attributes for the model.
+     *
+     * @return array
+     */
+    public function getHidden()
+    {
+        if (! $policy = Gate::getPolicyFor(self::class)) {
+            return $this->hidden;
+        }
+
+        return array_filter($this->hidden, function ($attribute) use ($policy) {
+            $ability = $this->getAttributeAbilityMethod($attribute);
+
+            if (is_callable([$policy, $ability])) {
+                return Gate::denies($ability, self::class);
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Get the method name for the attribute visibility ability in the model policy.
+     *
+     * @param  string  $attribute
+     * @return string
+     */
+    protected function getAttributeAbilityMethod($attribute)
+    {
+        return 'see'.ucfirst($attribute);
+    }
+}
