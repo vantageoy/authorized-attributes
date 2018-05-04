65a8dd383ff115014e29bac0d803c1ee263d32ac
Konstantin K <k2@sxw.ca>
2018-05-04 00:33:33 -0400

    Making hidden attributed non-fillable

--- src/AuthorizedAttributes.php
+++ src/AuthorizedAttributes.php
@@ -30,6 +30,43 @@ trait AuthorizedAttributes
     }
 
     /**
+     * Get the fillable attributes for the model.
+     *
+     * @return array
+     */
+    public function getFillable()
+    {
+        if (! $policy = Gate::getPolicyFor(self::class)) {
+            return $this->fillable;
+        }
+
+        return array_filter($this->fillable, function ($attribute) use ($policy) {
+            $ability = $this->getAttributeAbilityMethod($attribute);
+
+            if (is_callable([$policy, $ability])) {
+                return ! Gate::denies($ability, $this);
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Make the given, typically fillable, attributes non-fillable (but not guarded).
+     *
+     * @param  array|string  $attributes
+     * @return $this
+     */
+    public function makeNonFillable($attributes)
+    {
+        $attributes = is_array($attributes) ? $attributes : func_get_args();
+
+        $this->fillable = array_diff($this->fillable, (array) $attributes);
+
+        return $this;
+    }
+
+    /**
      * Get the method name for the attribute visibility ability in the model policy.
      *
      * @param  string  $attribute
